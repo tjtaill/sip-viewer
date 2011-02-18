de2c8a2c5ae0b9584344256e59a19a19aa653fdf
frederic.tardif <frederic.tardif@4c537ca7-3266-b9a2-309a-a2c459ed09ec>
2011-02-18 17:33:42 +0000

    few

---
+++ eclipse/plugin/src/javax/sip/viewer/eclipse/plugins/popup/actions/SipViewerPlugin.java
@@ -0,0 +1,94 @@
+package javax.sip.viewer.eclipse.plugins.popup.actions;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sip.viewer.SipTextViewer;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+
+public class SipViewerPlugin implements IObjectActionDelegate {
+
+  private Shell shell;
+  private ISelection selection;
+
+  /**
+   * Constructor for Action1.
+   */
+  public SipViewerPlugin() {
+    super();
+  }
+
+  /**
+   * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+   */
+  public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+    shell = targetPart.getSite().getShell();
+  }
+
+  /**
+   * @see IActionDelegate#run(IAction)
+   */
+  public void run(IAction action) {
+    try {
+      IStructuredSelection treeSelection = (IStructuredSelection) selection;
+      IResource lResource = (IResource) treeSelection.getFirstElement();
+   
+      SipTextViewer lSipTextViewer = new SipTextViewer();
+      lSipTextViewer.setParserClassName(SipLogAdapterParser.class.getCanonicalName());
+      List<String> lFilesURIs = new ArrayList<String>();
+      lFilesURIs.add(lResource.getLocation().toString());
+      lSipTextViewer.setFileNames(lFilesURIs);
+
+      MessageConsole console = findConsole("sip-viewer");
+      IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+      String id = IConsoleConstants.ID_CONSOLE_VIEW;
+      IConsoleView view = (IConsoleView) page.showView(id);
+      view.display(console);
+      MessageConsoleStream output = console.newMessageStream();
+      lSipTextViewer.display(console.newMessageStream());
+      output.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+      MessageDialog.openInformation(shell, "sip-viewer-plugin", e.toString());
+    }
+  }
+
+  private MessageConsole findConsole(String name) {
+    ConsolePlugin plugin = ConsolePlugin.getDefault();
+    IConsoleManager conMan = plugin.getConsoleManager();
+    IConsole[] existing = conMan.getConsoles();
+    for (int i = 0; i < existing.length; i++) {
+      if (name.equals(existing[i].getName())) {
+        return (MessageConsole) existing[i];
+      }
+    }
+    // no console found -> create new one
+    MessageConsole newConsole = new MessageConsole(name, null);
+    conMan.addConsoles(new IConsole[] { newConsole });
+    return newConsole;
+  }
+
+  public void selectionChanged(IAction action, ISelection selection) {
+    this.selection = selection;
+  }
+
+}
