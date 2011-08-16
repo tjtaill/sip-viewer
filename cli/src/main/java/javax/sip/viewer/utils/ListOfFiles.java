2e459d488223c0418c693c37f7253a3490c98d77
rochette.jonathan@gmail.com <rochette.jonathan@gmail.com@4c537ca7-3266-b9a2-309a-a2c459ed09ec>
2011-08-16 17:50:04 +0000

    dsffsd

--- cli/src/main/java/javax/sip/viewer/utils/ListOfFiles.java
+++ cli/src/main/java/javax/sip/viewer/utils/ListOfFiles.java
@@ -7,7 +7,7 @@ import java.util.NoSuchElementException;
 import java.util.zip.GZIPInputStream;
 
 public class ListOfFiles implements Enumeration {
-
+// test
   private String[] listOfFiles;
   private int current = 0;
 
